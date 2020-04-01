b7a2ed1c2620ab1189fe208e512f2801b1947943
eTrex-27 <62893226+eTrex-27@users.noreply.github.com>
2020-04-01 20:39:01 +0700

    Update cifar10.py

--- cifar10.py
+++ cifar10.py
@@ -9,86 +9,144 @@ from torchvision import datasets
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
-import PIL.ImageOps
-import requests
-from PIL import Image
+
 
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 16, 5, padding=2)
+        self.conv1 = nn.Conv2d(3, 30, 3, padding=1)
+        self.bn1 = nn.BatchNorm2d(30)
         self.relu1 = nn.ReLU()
-        self.pool1 = nn.MaxPool2d(2)
-        self.conv2 = nn.Conv2d(16, 20, 5, padding=2)
+        self.conv2 = nn.Conv2d(30, 30, 3, padding=1)
+        self.bn2 = nn.BatchNorm2d(30)
         self.relu2 = nn.ReLU()
-        self.pool2 = nn.MaxPool2d(2)
-        self.conv3 = nn.Conv2d(20, 20, 5, padding=2)
+        self.pool1 = nn.MaxPool2d(2)
+        self.conv3 = nn.Conv2d(30, 80, 3, padding=1)
+        self.bn3 = nn.BatchNorm2d(80)
         self.relu3 = nn.ReLU()
-        self.pool3 = nn.MaxPool2d(2)
-        self.fc = nn.Linear(4*4*20, 10)
+        self.conv4 = nn.Conv2d(80, 80, 3, padding=1)
+        self.bn4 = nn.BatchNorm2d(80)
         self.relu4 = nn.ReLU()
+        self.pool2 = nn.MaxPool2d(2)
+        self.conv5 = nn.Conv2d(80, 160, 3, padding=1)
+        self.bn5 = nn.BatchNorm2d(160)
+        self.relu5 = nn.ReLU()
+        self.conv6 = nn.Conv2d(160, 160, 3, padding=1)
+        self.bn6 = nn.BatchNorm2d(160)
+        self.relu6 = nn.ReLU()
+        self.pool3 = nn.MaxPool2d(2)
+        self.fc1 = nn.Linear(4 * 4 * 160, 4096)
+        self.relu7 = nn.ReLU()
+        self.fc2 = nn.Linear(4096, 1000)
+        self.relu8 = nn.ReLU()
+        self.fc3 = nn.Linear(1000, 10)
+        self.relu9 = nn.ReLU()
 
     def forward(self, x):
         y = self.conv1(x)
+        y = self.bn1(y)
         y = self.relu1(y)
-        y = self.pool1(y)
         y = self.conv2(y)
+        y = self.bn2(y)
         y = self.relu2(y)
-        y = self.pool2(y)
+        y = self.pool1(y)
         y = self.conv3(y)
+        y = self.bn3(y)
         y = self.relu3(y)
+        y = self.conv4(y)
+        y = self.bn4(y)
+        y = self.relu4(y)
+        y = self.pool2(y)
+        y = self.conv5(y)
+        y = self.bn5(y)
+        y = self.relu5(y)
+        y = self.conv6(y)
+        y = self.bn6(y)
+        y = self.relu6(y)
         y = self.pool3(y)
         y = y.view(y.shape[0], -1)
-        y = self.fc(y)
+        y = self.fc1(y)
+        y = self.relu7(y)
+        y = self.fc2(y)
+        y = self.relu8(y)
+        y = self.fc3(y)
         return y
 
-net = Net()
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+net = Net().to(device)
 print(net)
 
 loss_fn = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(net.parameters(), lr=0.01)
+optimizer = optim.Adam(net.parameters(), lr=0.0001)
 epochs = 20
+
+transform_train = transforms.Compose([transforms.RandomCrop(32, padding=4), transforms.RandomHorizontalFlip(), transforms.ToTensor(), transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),])
+transform_test = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),])
 
 if __name__ == '__main__':
     batch_size = 256
-    data_train = datasets.CIFAR10(root='./data', train=True, download=True, transform=transforms.Compose([transforms.Resize((32, 32)), transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))]))
-    data_test = datasets.CIFAR10(root='./data', train=False, download=True, transform=transforms.Compose([transforms.Resize((32, 32)), transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))]))
+    data_train = datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
+    data_test = datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
 
     data_train_loader = DataLoader(data_train, batch_size=batch_size, shuffle=True)
     data_test_loader = DataLoader(data_test, batch_size=batch_size)
 
     classes = ('plane', 'car', 'bird', 'cat', 'dear', 'dog', 'frog', 'horse', 'ship', 'truck')
-    iters, losses = [], []
-    n = 0
+    loss_train_history, correct_train_history, loss_test_history, correct_test_history = [], [], [], []
+    iters, iters2, losses_train, accuracies_train, losses_test, accuracies_test = [], [], [], [], [], []
+    n, k = 0, 0
     for _epoch in range(epochs):
-        loss = 0.0
-        correct = 0.0
+        train_loss = 0
+        correct = 0
+        total = 0
         for i, (images, labels) in enumerate(data_train_loader):
+            images, labels = images.to(device), labels.to(device)
             optimizer.zero_grad()
             output = net(images)
             loss = loss_fn(output, labels)
-            if i % 10 == 0:
-                print('Epoch: {}, batch: {}, loss: {}'.format(_epoch, i, loss))
-                print(net.fc.weight.detach().cpu().numpy().mean(axis=1))
-
             loss.backward()
             optimizer.step()
+
             iters.append(n)
-            losses.append(loss / batch_size)
+            losses_train.append(loss / batch_size)
             n += 1
 
-        total_correct = 0
-        avg_loss = 0.0
-        for i, (images, labels) in enumerate(data_test_loader):
-            output = net(images)
-            avg_loss += loss_fn(output, labels).sum()
-            pred = output.detach().max(1)[1]
-            total_correct += pred.eq(labels.view_as(pred)).sum()
-            avg_loss /= len(data_test)
-        print('Test Avg. Loss: %f, Accuracy: %f' % (avg_loss.detach().cpu().item(), float(total_correct) / len(data_test)))
+            train_loss += loss.item()
+            _, predicted = output.max(1)
+            total += labels.size(0)
+            correct += predicted.eq(labels).sum().item()
+            accuracies_train.append(100. * correct / total)
+        print('Epoch: %d | Loss_train: %.3f | Acc_train: %.3f%% (%d/%d)' % (_epoch, train_loss / len(data_train_loader), 100. * correct / total, correct, total))
 
-    plt.title("Training Curve")
-    plt.plot(iters, losses, label="Train")
-    plt.xlabel("Iterations")
-    plt.ylabel("Loss")
+        test_loss = 0
+        correct = 0
+        total = 0
+        with torch.no_grad():
+            for i, (images, labels) in enumerate(data_test_loader):
+                images, labels = images.to(device), labels.to(device)
+                output = net(images)
+                loss = loss_fn(output, labels)
+
+                iters2.append(k)
+                losses_test.append(loss / batch_size)
+                k += 1
+
+                test_loss += loss.item()
+                _, predicted = output.max(1)
+                total += labels.size(0)
+                correct += predicted.eq(labels).sum().item()
+                accuracies_test.append(100. * correct / total)
+            print('Epoch: %d | Loss_test: %.3f | Acc_test: %.3f%% (%d/%d)' % (_epoch, test_loss / (i + 1), 100. * correct / total, correct, total))
+
+    plt.plot(iters, losses_train, label='Training Loss')
+    plt.legend()
+    plt.show()
+    plt.plot(iters, accuracies_train, label='Training accuracy')
+    plt.legend()
+    plt.show()
+    plt.plot(iters2, losses_test, label='Test Loss')
+    plt.legend()
+    plt.show()
+    plt.plot(iters2, accuracies_test, label='Test accuracy')
+    plt.legend()
     plt.show()
